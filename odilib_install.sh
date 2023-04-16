ac07584182325aa77d3d0243e54aadb0215f41e1
Amit Sengupta <amit.ksengupta@gmail.com>
2023-04-16 10:58:47 +0200

    Adding dependent libs and connecting to odi

--- odilib_install.sh
+++ odilib_install.sh
@@ -1,3 +1,8 @@
 ODI_HOME=/Users/aksengup/apps/odiHome
 
 mvn install:install-file -Dfile=$ODI_HOME/odi/sdk/lib/odi-core.jar -DgroupId=oracle.odi -DartifactId=odi-core -Dversion=12.2.1.4 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=$ODI_HOME/odi/agent/lib/odi-standalone-agent.jar -DgroupId=oracle.odi -DartifactId=odi-standalone-agent -Dversion=12.2.1.4 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=$ODI_HOME/oracle_common/modules/oracle.toplink/eclipselink.jar -DgroupId=oracle.toplink -DartifactId=eclipselink -Dversion=1.0.0 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=$ODI_HOME/oracle_common/modules/oracle.jps/jps-api.jar -DgroupId=oracle.jps -DartifactId=jps-api -Dversion=1.0.0 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=$ODI_HOME/oracle_common/modules/oracle.odl/ojdl.jar -DgroupId=oracle.odl -DartifactId=ojdl -Dversion=1.0.0 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=$ODI_HOME/oracle_common/modules/oracle.idm/identitystore.jar -DgroupId=oracle.idm -DartifactId=identitystore -Dversion=1.0.0 -Dpackaging=jar -DgeneratePom=true
--- src/main/java/am/odi/OdiProcess.java
+++ src/main/java/am/odi/OdiProcess.java
@@ -1,4 +1,33 @@
 package am.odi;
 
+import oracle.odi.core.OdiInstance;
+import oracle.odi.core.config.MasterRepositoryDbInfo;
+import oracle.odi.core.config.OdiInstanceConfig;
+import oracle.odi.core.config.PoolingAttributes;
+import oracle.odi.core.config.WorkRepositoryDbInfo;
+import oracle.odi.core.security.Authentication;
+
 public class OdiProcess {
+
+    public static void main(String[] args) {
+        String driver = "oracle.jdbc.OracleDriver";
+        String sourceUrl = "jdbc:oracle:thin:@localhost:1521/odipdb.localdomain";
+        String sourceSchema = "DEV_ODI_REPO";
+        String sourceSchemaPwd = "Welcome1";
+        String sourceWorkrep = "WORKREP";
+        String sourceOdiUser = "SUPERVISOR";
+        String sourceOdiUserPwd = "Welcome1";
+
+        System.out.println("Hello world from odi process!!!!");
+
+        MasterRepositoryDbInfo sourceMasterInfo = new MasterRepositoryDbInfo(sourceUrl, driver, sourceSchema, sourceSchemaPwd.toCharArray(), new PoolingAttributes());
+        WorkRepositoryDbInfo sourceWorkInfo = new WorkRepositoryDbInfo(sourceWorkrep, new PoolingAttributes());
+
+        OdiInstance sourceOdiInstance = OdiInstance.createInstance(new OdiInstanceConfig(sourceMasterInfo, sourceWorkInfo));
+        Authentication sourceAuth= sourceOdiInstance.getSecurityManager().createAuthentication(sourceOdiUser, sourceOdiUserPwd.toCharArray());
+        sourceOdiInstance.getSecurityManager().setCurrentThreadAuthentication(sourceAuth);
+        System.out.println("Connected to Source! Yay!");
+
+
+    }
 }
